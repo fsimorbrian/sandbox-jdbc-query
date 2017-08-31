20d4c465d62be43648b7c1d9e0440165259a4404
Brian Moriarty <bmoriarty@forwardslope.com>
2017-08-30 19:02:44 -0700

    updated docker and root.xml variables

--- src/main/docker/setenv.sh
+++ src/main/docker/setenv.sh
@@ -2,12 +2,34 @@
 
 export JAVA_HOME=/usr/java/latest
 
-sed "s/\${DOC_DB_SERVICE_NAME}/${DOC_DB_SERVICE_NAME}/" /usr/local/src/ROOT.xml \
-| sed "s/\${DOC_POSTGRESQL_DATABASE}/${DOC_POSTGRESQL_DATABASE}/" \
-| sed "s/\${DOC_POSTGRESQL_USER}/${DOC_POSTGRESQL_USER}/" \
-| sed "s/\${DOC_POSTGRESQL_PASSWORD}/${DOC_POSTGRESQL_PASSWORD}/" \
-| sed "s/\${OBJ_DB_SERVICE_NAME}/${OBJ_DB_SERVICE_NAME}/" /usr/local/src/ROOT.xml \
-| sed "s/\${OBJ_POSTGRESQL_DATABASE}/${OBJ_POSTGRESQL_DATABASE}/" \
-| sed "s/\${OBJ_POSTGRESQL_USER}/${OBJ_POSTGRESQL_USER}/" \
-| sed "s/\${OBJ_POSTGRESQL_PASSWORD}/${OBJ_POSTGRESQL_PASSWORD}/" \
+resolve() {
+    #
+    # Check variable is set, exit with error if not, otherwise return value.
+    # Params:
+    # 1: string name of variable to check.
+    # return: value of named variable
+    # usage: NEWVAR=$(resolve VARNAME)
+    #
+    local CHECK=`echo $1`
+    if [ -z ${!CHECK} ]; then echo "ERROR: env_var $CHECK value is required"; exit 1; fi
+
+    echo ${!CHECK}
+}
+
+sed "s/\${DATA_DB_SERVICE_HOST}/$(resolve DATA_DB_SERVICE_HOST)/" /usr/local/src/ROOT.xml \
+| sed "s/\${DATA_DB_SERVICE_PORT}/$(resolve DATA_DB_SERVICE_PORT)/" \
+| sed "s/\${DATA_DB_USERNAME}/$(resolve DATA_DB_USERNAME)/" \
+| sed "s/\${DATA_DB_PASSWORD}/$(resolve DATA_DB_PASSWORD)/" \
+| sed "s/\${DATA_DB_NAME}/$(resolve DATA_DB_NAME)/" \
+| sed "s/\${DATA_DB_MAX_TOTAL}/$(resolve DATA_DB_MAX_TOTAL)/" \
+| sed "s/\${DATA_DB_MAX_IDLE}/$(resolve DATA_DB_MAX_IDLE)/" \
+| sed "s/\${DATA_DB_MAX_WAIT}/$(resolve DATA_DB_MAX_WAIT)/" \
+| sed "s/\${LOB_DB_SERVICE_HOST}/$(resolve LOB_DB_SERVICE_HOST)/" \
+| sed "s/\${LOB_DB_SERVICE_PORT}/$(resolve LOB_DB_SERVICE_PORT)/" \
+| sed "s/\${LOB_DB_USERNAME}/$(resolve LOB_DB_USERNAME)/" \
+| sed "s/\${LOB_DB_PASSWORD}/$(resolve LOB_DB_PASSWORD)/" \
+| sed "s/\${LOB_DB_NAME}/$(resolve LOB_DB_NAME)/" \
+| sed "s/\${LOB_DB_MAX_TOTAL}/$(resolve LOB_DB_MAX_TOTAL)/" \
+| sed "s/\${LOB_DB_MAX_IDLE}/$(resolve LOB_DB_MAX_IDLE)/" \
+| sed "s/\${LOB_DB_MAX_WAIT}/$(resolve LOB_DB_MAX_WAIT)/" \
 > /opt/webserver/conf/Catalina/localhost/ROOT.xml
